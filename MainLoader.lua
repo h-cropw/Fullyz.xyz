4e87a3f613f23fe404455a8f9271712f04249e38
h-cropw <110641999+h-cropw@users.noreply.github.com>
2024-12-20 18:44:32 +0100

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,4 +1,5 @@
 print("The Fullyz.xyz development has been resumed! 14/12/2024")
 print("https://discord.gg/ABP6aXf5PZ")
+print("Fullyz.xyz temporarily to Nexar! discord.gg/Nexar ")
 --setclipboard("https://discord.gg/ABP6aXf5PZ")
 --game:GetService("Players").LocalPlayer:Kick("The Fullyz.xyz development has been resumed, copied the discord invite into your clipboard!")
