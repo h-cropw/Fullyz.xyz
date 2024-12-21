9b3069227aa55beee987a659021624d4c2d748dc
h-cropw <110641999+h-cropw@users.noreply.github.com>
2024-12-21 17:13:16 +0100

    Update Version.lua

--- Version.lua
+++ Version.lua
@@ -1,4 +1,11 @@
-local SVAL_Version = {
-    version = "3.6.0 BETA BUILD",
+local Versions = {
+    SVAL = {
+        version = "3.6.0 BETA BUILD",
+    },
+    Nexar = {
+        version = "3.8.1",
+    },
+    Fullyz_xyz = {
+        version = "0.0.5 PRIVATE BUILD",
+    }
 }
-
