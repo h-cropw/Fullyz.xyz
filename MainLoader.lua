e69ad4d19491f29e6f02edf5502f872cebdeb412
h-cropw <110641999+h-cropw@users.noreply.github.com>
2025-03-03 15:35:57 +0100

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,4 +1,5 @@
-print("The Fullyz.xyz development has been resumed! 12/01/2025")
-print("https://discord.gg/ABP6aXf5PZ")
-setclipboard("https://discord.gg/ABP6aXf5PZ")
-game:GetService("Players").LocalPlayer:Kick("The Fullyz.xyz development has been resumed, copied the discord invite into your clipboard!")
+if game.PlaceId == 2512643572 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/h-cropw/Fullyz.xyz/refs/heads/main/Games/BubbleGumSimulator.lua"))()
+else
+    print("[Fullyz.xyz] Game not supported")
+end
